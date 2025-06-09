c5fa3ea3acbc4ecb4fd0df7c055c0e07efcb43dd
Alessandro Averna <alessandro.averna.1994@gmail.com>
2025-06-09 22:39:38 +0200

    feat: add UpdatedAtUtc default when saving changes WIP (to test)

--- webapi-aspnet8/src/YourProjectName.Infrastructure/Persistence/ApplicationDbContext.cs
+++ webapi-aspnet8/src/YourProjectName.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -2,15 +2,38 @@
 using Microsoft.EntityFrameworkCore;
 using YourProjectName.Application.Infrastructure.Persistance;
 using YourProjectName.Domain.WeatherForecasts;
+using YourProjectName.Shared.Domain;
+using YourProjectName.Shared.Time;
 
 namespace YourProjectName.Infrastructure.Persistence
 {
-    internal class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IUnitOfWork
+    internal class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IDateTimeProvider dateTimeProvider) : DbContext(options), IUnitOfWork
     {
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            SetUpdatedAtUtcOnEveryAuditableEntity();
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void SetUpdatedAtUtcOnEveryAuditableEntity()
+        {
+            var entitiesBeignUpdated = ChangeTracker.Entries<AuditableEntity>()
+                .Where(entry => entry.State == EntityState.Modified);
+
+            foreach (var entry in entitiesBeignUpdated)
+            {
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAtUtc = dateTimeProvider.UtcNow;
+                }
+            }
         }
 
         public DbSet<WeatherForecast> Forecasts { get; set; }
